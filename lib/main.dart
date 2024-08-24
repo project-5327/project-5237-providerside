39eb9a9f8eb2e2732f5420d3f9bb2e1517fac092
shravya462 <shravyapoojary462@gmail.com>
2024-08-24 17:50:24 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
             ),
             useMaterial3: true,
           ),
-          home: ProjectDetailScreen(),
+          home: DashBoardView(),
         );
 
         //  Profile3());
